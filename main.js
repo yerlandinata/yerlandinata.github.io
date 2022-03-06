9bed0f2ecbeded6bc382ac1eb977964488783cdd
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2022-03-06 19:38:21 +0700

    Implement tax v2

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 const million = 1000 * 1000
 const nonTaxableIncome = 50 * million
+const MARRIAGE_STATUS = {
+    SINGLE: 'Lajang',
+    MARRIED: 'Menikah',
+    DIVORCED: 'Cerai'
+}
+
+const TAX_INCENTIVES = {
+    MARRIED: 10 * million,
+    SINGLE_PARENTS: 20 * million,
+    PER_CHILD: 15 * million
+}
 
 function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
     console.log(annualIncome)
-    const taxableIncome = annualIncome - nonTaxableIncome
+    let taxDeduction = nonTaxableIncome
+    
+    if (marriageStatus == MARRIAGE_STATUS.MARRIED) {
+        taxDeduction += TAX_INCENTIVES.MARRIED
+    } else if (marriageStatus != MARRIAGE_STATUS.MARRIED && dependentChildrenCount > 0) {
+        taxDeduction += TAX_INCENTIVES.SINGLE_PARENTS
+    }
+
+    taxDeduction += (dependentChildrenCount * TAX_INCENTIVES.PER_CHILD)
+
+    const taxableIncome = annualIncome - taxDeduction
+
 
     if (taxableIncome <= 0) {
         return 0
