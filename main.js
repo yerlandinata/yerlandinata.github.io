e6e7754787fa1b671aea795aef7e77195f673167
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2022-03-06 19:16:08 +0700

    Implement tax-1

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
+const million = 1000 * 1000
+const nonTaxableIncome = 50 * million
+
 function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
-    console.log(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount)
-    return 500000
+    console.log(annualIncome)
+    const taxableIncome = annualIncome - nonTaxableIncome
+
+    if (taxableIncome <= 0) {
+        return 0
+    }
+
+    if (taxableIncome < 200 * million) {
+        return taxableIncome * .10
+    }
+    if (taxableIncome < 450 * million) {
+        return taxableIncome * .15
+    }
+    return taxableIncome * .20
 }
